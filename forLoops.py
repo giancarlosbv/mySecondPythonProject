896e2bd8224bd2393fa4f1ef3a94928ab7472181
Giancarlos Blanco <giancarlosbv@gmail.com>
2021-11-01 11:58:50 -0400

    second commit

--- forLoops.py
+++ forLoops.py
@@ -3,7 +3,7 @@ numbers = [1,2,3]
 
 for i in range(len(numbers)):
     # print(i+1)
-    print(numbers[i])
+    print(numbers[i],"hello")
 print()
 
 for i in range(x):   #range() is a method for starting and ending a number interval
